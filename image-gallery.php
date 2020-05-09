475922de416cb618e5720f6d3da68b05e9eeecda
MichaelLionHart <m.hart@students.clark.edu>
2020-05-09 12:12:12 -0600

    latest updates

--- image-gallery.php
+++ image-gallery.php
@@ -1,5 +1,6 @@
-<?php require_once 'inc/functions.inc.php'; ?>
-
+<?php
+        require_once 'inc/logic.inc.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,28 +8,72 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lab 4 - Image Gallery</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS (CDN)
+    <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="container px-0">
-        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-            <input type="hidden" name="MAX_FILE_SIZE" value="100000000">
-            <input class="form-control-file" type="file" name="file_upload">
-            <input class="btn btn-primary" type="submit" name="submit" value="Upload Image">
-        </form>
-    </div>
+<div class="container main">
+        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label class="control-label"><h1>Upload Your Favorite Images</h1></label>
+                            <div class="preview-zone hidden">
+                                <div class="box box-solid">
+                                    <div class="box-header with-border">
+                                        <div><b>Preview</b></div>
+                                        <div class="box-tools pull-right">
+                                            <button type="button" class="btn btn-danger btn-xs remove-preview">
+                                                <i class="fa fa-times"></i> Reset This Form
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="box-body"></div>
+                                </div>
+                            </div>
+                            <div class="dropzone-wrapper">
+                                <div class="dropzone-desc">
+                                    <i class="glyphicon glyphicon-download-alt"></i>
+                                    <p>Choose an image file</p>
+                                </div>
+                                <input type="file" name="file_upload" class="dropzone">
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-    <div class="container special">
-        <div class="row">
-            <div class="col-12 d-flex flex-wrap flex-row align-items-center justify-content-between">
-                <?php display_images(); ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary pull-right">Upload</button>
+                        <?php
+                        if (!empty($message)) {
+                            echo "<p id=\"alert\" class=\"alert alert-primary mt-4\">{$message}</p>";
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+        <div class="container special">
+            <div class="row">
+                <div class="col-12 d-flex flex-wrap flex-row align-items-between justify-content-center">
+                    <?php display_images(); ?>
+                </div>
             </div>
         </div>
     </div>
 
-    <?php
-        require_once 'inc/logic.inc.php';
-    ?>
+    
+
+    <script src="js/script.js"></script>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    
 </body>
 </html>
--- inc/logic.inc.php
+++ inc/logic.inc.php
@@ -27,15 +27,31 @@
         }
     }
 
-    // move the file
-    if (isset($_GET['file'])) {
-        copy('uploads/' . $_GET['file'], 'backup/' . $_GET['file']);
-
-        if (unlink('uploads/' . $_GET['file'])) {
+    if (isset($_GET['file'])){
+        copy('uploads/' . $_GET['file'],'backup/'.$_GET['file']);
+        // insert code from tuesday to move the file somewhere else instead of deleting
+        if(unlink("uploads/" . $_GET['file'])){
             header('Location: image-gallery.php');
         } else {
-            echo '<p>File could not be deleted.</p>';
+            echo '<p>Sorry, I could not delete the file you selected.</p>';
         }
+    }
+
+    function display_images() {
+        // starting at current directory
+        $dir = "uploads";
+        if (is_dir($dir)) {
+            if ($dir_handle = opendir($dir)) {
+                while ($filename = readdir($dir_handle)) {
+                    if (!is_dir($filename) && $filename != '.DS_Store') {
+                        $filename = urlencode($filename);
+                        echo "<div><img src=\"uploads/$filename\" alt=\"A photo\"><a class=\"btn text-info\" href=\"?file=$filename\">Delete picture</a></div>";
+                    }
+                } // end while
+                // close directory now that we no longer need it
+                closedir($dir_handle);
+            } // end if
+        } // end if
     }
 
     
