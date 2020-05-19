81f16db33ed5374fd941783b95ae6e6cbae2039f
MichaelLionHart <m.hart@students.clark.edu>
2020-05-18 23:14:23 -0600

    Fixed logout button

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -18,5 +18,5 @@
     <li><a href="image-gallery.php">Image Gallery</a></li>
     <li><a href="register.php">Register</a></li>
     <li><a href="login.php" id="login">Login</a></li>
-    <li><a href="login.php" id="logout">Logout</a></li>
+    <li><a href="home.php" id="logout">Logout</a></li>
 </ul>
